304d5a55cbbc6715157edd9cf66d7031d4ed6a70
devmango1128 <devmango1128@gmail.com>
2023-05-08 23:04:06 +0900

    layout 작업

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 let LOTTO = {
+    init : function() {
+
+    },
     number_create : function(type) {
         alert('lotto 자동생성');
     }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    LOTTO.init();
+});
