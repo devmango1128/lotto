bf44d34fda22c31fef21c4b7faab298f1a119d3d
devmango1128 <devmango1128@gmail.com>
2023-05-09 23:07:24 +0900

    레이아웃 작업중

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,15 @@ let LOTTO = {
     init : function() {
 
     },
-    number_create : function(type) {
+    number_create : function(obj, type) {
+        let menus = document.getElementsByClassName("menu");
+
+        for(let i = 0; i < menus.length; i++) {
+            let menu = menus.item(i);
+            menu.classList.remove('active')
+        }
+
+        obj.classList.toggle('active', true);
         alert('lotto 자동생성');
     }
 }
