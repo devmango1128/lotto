69e8866491e7bf5db5a8c2ec158602074e0bf505
devmango1128 <devmango1128@gmail.com>
2023-05-15 22:01:46 +0900

    마지막 회차 구해오기

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,41 @@
 let LOTTO = {
+    latestRound : 1,
     init : function() {
-
+        this.fn_lotto_last_turn_number();
     },
-    number_create : function(obj, type) {
-        let menus = document.getElementsByClassName("menu");
+    fn_lotto_last_turn_number : function() {
 
-        for(let i = 0; i < menus.length; i++) {
-            let menu = menus.item(i);
-            menu.classList.remove('active')
+        const currentDate = new Date();
+        const currentDay = currentDate.getDay();
+
+        const saturday = 6; // 토요일은 6번째 요일
+
+        // 현재 요일이 토요일이 아닌 경우, 가장 최근의 토요일로 이동
+        if (currentDay !== saturday) {
+            const diff = currentDay > saturday ? currentDay - saturday : (7 - saturday) + currentDay;
+            currentDate.setDate(currentDate.getDate() - diff);
         }
 
-        obj.classList.toggle('active', true);
-        alert('lotto 자동생성');
+        const baseDate = new Date('2002-12-07'); // 한국 로또 시작 : 2002년 12월 7일 토요일
+        const diffDays = Math.floor((currentDate - baseDate) / (1000 * 60 * 60 * 24));
+        const latestRound = Math.floor(diffDays / 7) + 1;
+
+        this.latestRound = latestRound;
+        console.log(latestRound);
+        this.fn_lotto_turn();
+    },
+    fn_lotto_turn : function() {
+        let _this = this;
+        const lottoTurn = document.getElementById("lottoTurn");
+
+        for (let i = _this.latestRound; i > 1; i--) {
+
+            let newOption = document.createElement("option");
+            newOption.value = i;
+            newOption.text = i + '회';
+
+            lottoTurn.appendChild(newOption);
+        }
     }
 }
 
