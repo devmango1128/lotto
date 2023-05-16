56ad445a8c93d87793857980d9043a508bbdec5a
devmango1128 <devmango1128@gmail.com>
2023-05-16 22:23:07 +0900

    lotto 번호 받아오기

--- src/js/index.js
+++ src/js/index.js
@@ -21,7 +21,6 @@ let LOTTO = {
         const latestRound = Math.floor(diffDays / 7) + 1;
 
         this.latestRound = latestRound;
-        console.log(latestRound);
         this.fn_lotto_turn();
     },
     fn_lotto_turn : function() {
@@ -36,7 +35,49 @@ let LOTTO = {
 
             lottoTurn.appendChild(newOption);
         }
+    },
+    fn_lotto_turn_change : function() {
+        let _this = this;
+
+        const selectElement = document.getElementById("lottoTurn");
+        const selectedValue = selectElement.value;
+
+        console.log(selectedValue); // 선택된 옵션의 값 출력
+
+        // var url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=' + selectedValue;
+        //
+        // $.ajax({
+        //     url: url,
+        //     method: 'GET',
+        //     dataType: 'jsonp',
+        //     jsonpCallback: 'processLottoData',
+        //     success: function(response) {
+        //         console.log(response);
+        //         // 응답 데이터를 처리하는 코드 작성
+        //     },
+        //     error: function(xhr, status, error) {
+        //         console.error(error);
+        //     }
+        // });
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=' + selectedValue, true); // AJAX 요청 설정
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const response = xhr.responseText;
+                // AJAX 요청이 완료되고 응답이 성공적으로 도착한 경우에 실행할 코드
+                console.log(response);
+            }
+        };
+
+        xhr.send(); // AJAX 요청 보내기
     }
+}
+
+function processLottoData(data) {
+    // 로또 데이터를 처리하는 코드 작성
+    console.log(data);
 }
 
 document.addEventListener("DOMContentLoaded", function(){
