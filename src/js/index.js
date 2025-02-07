50f8e40a710c109cc09a7f158649759ab54a038e
devmango1128 <devmango1128@gmail.com>
2025-02-07 23:55:19 +0900

    폰트, 화면 크기 조정

--- src/js/index.js
+++ src/js/index.js
@@ -1186,9 +1186,9 @@ let LOTTO = {
     //폰트사이즈변경
     , fn_font_size_change: function(factor) {
         let html = document.documentElement;
-        alert((parseInt(getComputedStyle(html).fontSize) + factor) + '/' + (parseInt(html.dataset.fontSize) + factor));
-        html.dataset.fontSize = parseInt(html.dataset.fontSize) + factor;
-        html.style.fontSize = (parseInt(getComputedStyle(html).fontSize) + factor) + 'px';
+        alert(getComputedStyle(html).fontSize + factor);
+        html.dataset.fontSize = getComputedStyle(html).fontSize + factor;
+        html.style.fontSize = getComputedStyle(html).fontSize + factor + 'px';
     }
     //화면사이즈변경
     , fn_view_size_change : function(factor) {
